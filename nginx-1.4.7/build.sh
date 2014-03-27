17d15a37170a06bfbbe9ec2caf91efbb45ee272d
leo <yuan.yi.coding@gmail.com>
2014-03-27 23:37:12 +0800

    Update build.sh
    support add include path and lib, and replace CC LINK to g++

--- nginx-1.4.7/build.sh
+++ nginx-1.4.7/build.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
-./configure --prefix=./webserver/ --add-module=./src/aret/
-cp objs_Makefile objs/Makefile
-make -j8
+
+INSTALL_PATH="./webserver"
+INCLUDE_PATH="-I./src/aret"
+LD_PATH="-ltera_easy -ljson -L./src/aret"
+./configure --prefix=$INSTALL_PATH --add-module=./src/aret/ --with-cc-opt="$INCLUDE_PATH" --with-ld-opt="$LD_PATH";
+
+# update g++
+sed -i '1087s/\$(CC)/g++/' objs/Makefile;
+sed -i '215s/\$(LINK)/g++/' objs/Makefile; 
+
+#cp objs_Makefile objs/Makefile
+make -j8a && make install
